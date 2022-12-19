2a0922e18858fd7f7c8667ea4199dc023e709859
aylancOnur <aylanc.onur@gmail.com>
2022-12-19 17:37:40 +0300

    screens and navigation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {View} from 'react-native';
+import {Navigation} from './navigation';
+
+const App = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Navigation />
+    </View>
+  );
+};
+
+export {App};
---
+++ src/navigation/stacks/mainStack.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {Home, Detail} from '../../screens';
+
+const Stack = createNativeStackNavigator();
+
+const MainStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        options={{headerShown: false}}
+        name="Home"
+        component={Home}
+      />
+      <Stack.Screen
+        options={{headerShown: false}}
+        name="Detail"
+        component={Detail}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export {MainStack};
